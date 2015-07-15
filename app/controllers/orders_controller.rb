c41df644ce902090e6ee3b03762561cae4629abc
amirahaile <amira.dhaile@gmail.com>
2015-07-15 14:06:21 -0700

    Added Orders#show view. Started fleshing out shopping cart page.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+
+  def new
+    # triggered by the 'add to cart' button on Products#show
+  end
+
+  def create
+    # see :new
+  end
+
+  def show
+    order = Order.find(params[:id])
+    @order_items = order.order_items
+  end
 end
