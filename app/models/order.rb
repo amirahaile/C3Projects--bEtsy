c39233d5e399a423a4c01c59dee657d5f2d6afd4
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-16 13:43:57 -0700

    Working on Models & Tests for Category, Order, & Review.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,84 @@
 class Order < ActiveRecord::Base
+  # Associations
   has_many :order_items
+
+  # Validations
+  validates :email, presence: true
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true, length: { is: 2 } #, must be two (capital) characters? ex. WA
+  validates :zipcode, presence: true #, must be five-ten characters? ex. 55555-5555
+  validates :card_last_4, presence: true, length: { is: 4 }
+  validates :card_exp, presence: true #, must be in the future?
+  validates :status, presence: true, inclusion:
+                                     { in: %w(pending paid cancelled complete),
+                                       message: "%{value} is not a valid status" }
+
+  # Callbacks
+  before_validation :capitalize_city!, :state_conversion!
+
+  def capitalize_city!
+    return unless self.city == city.capitalize
+
+    self.city.capitalize!
+  end
+
+  states = {
+    "AL" => "Alabama"
+    "AK" => "Alaska"
+    "AZ" => "Arizona"
+    "AR" => "Arkansas"
+    "CA" => "California"
+    "CO" => "Colorado"
+    "CT" => "Connecticut"
+    "DE" => "Delaware"
+    "FL" => "Florida"
+    "GA" => "Georgia"
+    "HI" => "Hawaii"
+    "ID" => "Idaho"
+    "IL" => "Illinois"
+    "IN" => "Indiana"
+    "IA" => "Iowa"
+    "KS" => "Kansas"
+    "KY" => "Kentucky"
+    "LA" => "Louisiana"
+    "ME" => "Maine"
+    "MD" => "Maryland"
+    "MA" => "Massachusetts"
+    "MI" => "Michigan"
+    "MN" => "Minnesota"
+    "MS" => "Mississippi"
+    "MO" => "Missouri"
+    "MT" => "Montana"
+    "NE" => "Nebraska"
+    "NV" => "Nevada"
+    "NH" => "New Hampshire"
+    "NJ" => "New Jersey"
+    "NM" => "New Mexico"
+    "NY" => "New York"
+    "NC" => "North Carolina"
+    "ND" => "North Dakota"
+    "OH" => "Ohio"
+    "OK" => "Oklahoma"
+    "OR" => "Oregon"
+    "PA" => "Pennsylvania"
+    "RI" => "Rhode Island"
+    "SC" => "South Carolina"
+    "SD" => "South Dakota"
+    "TN" => "Tennessee"
+    "TX" => "Texas"
+    "UT" => "Utah"
+    "VT" => "Vermont"
+    "VA" => "Virginia"
+    "WA" => "Washington"
+    "WV" => "West Virginia"
+    "WI" => "Wisconsin"
+    "WY" => "Wyoming"
+  }
+
+  def state_conversion!
+    return if self.state.states.keys.includes?(self.state)
+
+
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,12 @@
 class Review < ActiveRecord::Base
+  # Associations
+  belongs_to :product
+
+  # Validations
+  validates :rating, presence: true, numericality: { only_integer: true },
+                     format: { with: /[1-5]/ } # STILL WORKING ON
+  validates :product_id, presence: true
+
+  # Scopes
+  # MAYBE DISPLAYING ON RATINGS BY 1, 2, 3, 4, OR 5.
 end
