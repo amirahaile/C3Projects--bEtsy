a5ea5b062e4f0f9420443a6f0a0d258742cdba08
amirahaile <amira.dhaile@gmail.com>
2015-07-15 17:41:50 -0700

    Removed the requirement of payment information.

--- db/migrate/20150714215552_create_orders.rb
+++ db/migrate/20150714215552_create_orders.rb
@@ -1,14 +1,14 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.string :email, null: false
-      t.string :address1, null: false
+      t.string :email
+      t.string :address1
       t.string :address2
-      t.string :city, null: false
-      t.string :state, null: false
-      t.string :zipcode, null: false
-      t.string :card_last_4, null: false
-      t.datetime :card_exp, null: false
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.string :card_last_4
+      t.datetime :card_exp
       t.string :status, null: false, default: "pending"
       t.timestamps null: false
     end
