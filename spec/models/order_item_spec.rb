3439770a357a7b24792150993ee5723aee542e9d
amirahaile <amira.dhaile@gmail.com>
2015-07-15 19:57:03 -0700

    Wrote specs for :quantity validations.

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+
+    # how can I test for presence when there's a default value?
+    it "defaults :quantity to 1" do
+      order_item = OrderItem.new(order_id: 1, product_id: 1)
+
+      expect(order_item.save).to be true
+      expect(order_item.quantity).to eq 1
+    end
+
+    it "doesn't allow :quantity to be a string" do
+      order_item = OrderItem.new(quantity: 'none', order_id: 1, product_id: 1)
+      expect(order_item.save).to be false
+    end
+
+    it "doesn't allow :quantity to be less than 0" do
+      order_item = OrderItem.new(quantity: -10, order_id: 1, product_id: 1)
+      expect(order_item.save).to be false
+    end
+  end
 end
