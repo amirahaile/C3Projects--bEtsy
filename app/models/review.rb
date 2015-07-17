40f9d324547f8275ed81014406a1693c35d86d56
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-17 13:34:26 -0700

    Got closer to figuring out rspecs for Order.

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,16 +4,13 @@ class Review < ActiveRecord::Base
 
   # Validations
   validates :rating, presence: true, numericality: { only_integer: true },
-                     format: { with: /[1-5]/ } # STILL WORKING ON
-  validates :product_id, presence: true
+                    inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :product_id, presence: true, numericality: { only_integer: true }
 
   # Scopes
-  # NEED TO TEST:
-  # scope :ratings_by_1, -> { where(rating: 1) }
-  # scope :ratings_by_2, -> { where(rating: 2) }
-  # scope :ratings_by_3, -> { where(rating: 3) }
-  # scope :ratings_by_4, -> { where(rating: 4) }
-  # scope :ratings_by_5, -> { where(rating: 5) }
-
-  # MAYBE DISPLAYING ON RATINGS BY 1, 2, 3, 4, OR 5.
+  scope :ratings_by_1, -> { where(rating: 1) }
+  scope :ratings_by_2, -> { where(rating: 2) }
+  scope :ratings_by_3, -> { where(rating: 3) }
+  scope :ratings_by_4, -> { where(rating: 4) }
+  scope :ratings_by_5, -> { where(rating: 5) }
 end
