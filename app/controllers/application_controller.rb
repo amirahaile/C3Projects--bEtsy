58a050a8812964a2775186896ff2e0c0d2646ce2
amirahaile <amira.dhaile@gmail.com>
2015-07-21 13:42:41 -0700

    Guard from false errors.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,15 @@ class ApplicationController < ActionController::Base
     redirect_to login_path, flash: { error: "Merchant login required" } unless session[:user_id]
   end
 
-  # on Products#index the cart badge goes back to 0
   def logged_in
     @user = User.find(session[:user_id]) unless session[:user_id].nil?
     @username = @user ? @user.username : "Guest"
-    @order = Order.find(session[:order_id])
-    @cart_quantity = quantity_in_cart(@order)
+
+    # guards from errors when order hasn't been initalized yet
+    if session[:order_id] != nil
+      @order = Order.find(session[:order_id])
+      @cart_quantity = quantity_in_cart(@order)
+    end
   end
 
   private
