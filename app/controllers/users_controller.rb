e53fdfa9bd2b3e7976a9355f82f2d9541ed4fde6
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-21 13:18:02 -0700

    WIP of user#new & user#create.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,28 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
 
-    redirect_to user_path(@user)
+    # let the model check if it successfuly passes validations and saves
+      # if not, send a flash error depedning on what errors are thrown?
+    if @user.save
+      session[:user_id] = @user.id # creates a session - they are logged in
+      redirect_to user_path(@user) # WHERE DO WE WANT THIS TO REDIRECT TO?
+      # redirect_to root_path # with a message that they successfully created an account
+    else
+      flash.now[:errors] = "ERROR"
+        # Would we use the flash.now to display the individual errors.
+        # Would we reference the individual errors via the insance variable?
+          # long_message = ""
+          # count = 0
+          # while (@user.errors.messages[:password].count - 1)
+
+          # @user.errors.messages[:password].each do |message|
+          #   long_message = message + "&"
+          # end
+      # raise
+      render :new
+    end
+
   end
 
   def edit
@@ -73,7 +92,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:review).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
 end
