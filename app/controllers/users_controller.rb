8d57776c5e28306f5a85075879a52f9ea3703b3d
Kyna <khn710@gmail.com>
2015-07-21 16:26:22 -0700

    WIP: user orders index and show..start to reffactor to use has_many_through??

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,44 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:index, :show]
-  after_action :products_in_orders, only: :products_of_user
+  before_action :find_user, only: :products_of_user
+  before_action :products_of_user, only: [:index, :show]
 
   def find_user
     @user = User.find(session[:user_id])
   end
 
   def products_of_user
-    user_products = @user.products
+    user_products = @user.products.to_a
     @product_ids = []
     user_products.each do |product|
       @product_ids << product.id
     end
-    return @product_ids
+    order_items_by_user
   end
 
   def order_items_by_user
     @order_items = []
     @product_ids.each do |product_id|
-      @order_items << OrderItems.where(product_id: product_id)
+      @order_items << OrderItem.where(product_id: product_id)
     end
-    return @order_items
+    if @order_items.count > 1
+      return @order_items
+    else
+      @order_items = nil
+    end
   end
 
   def index
     # if orders.order_items.product_id == user.products.ids
     @orders = []
-    @order_items.each do |order_item|
-      @orders << order_item.order
-      @orders.uniq!
+    if @order_items.nil?
+      puts "No Current Orders"
+    else
+      @order_items.each do |order_item|
+        @orders << Order.where(id: order_item.first.order_id)
+        # still need to account for qty of order item
+        @orders.uniq!
+      end
     end
-    return @orders
   end
 
   def show
