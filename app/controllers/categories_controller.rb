4ee9f85d1962acc06120e58286c63e952b661f9d
Sally Moore <sallyamoore@gmail.com>
2015-07-21 22:17:05 -0700

    Finished #new and #create for category. Finished new category view. Added a callback to normalize names to titlecase in Category model.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,28 @@
 class CategoriesController < ApplicationController
   before_action :require_login
 
-
   def new
-
+    index
+    @category = Category.new
   end
 
   def create
-    
+    @category = Category.create(create_params)
+    if @category.save
+      redirect_to new_category_path, notice: "Category added!"
+    else
+      flash.now[:error] = "Could not add category. Are you sure you aren't duplicating a category that already exists?"
+      render :new
+    end
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  private
+
+  def create_params
+    params.require(:category).permit(:name)
   end
 end
