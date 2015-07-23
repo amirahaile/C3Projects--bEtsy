dcf1d5d6c53c4a8d53bc0829b2efaa0a94170be3
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-07-23 16:32:26 -0700

    Removing login page.

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,6 @@ Rails.application.routes.draw do
 
   resources :categories, only: [ :new, :create ]
 
-  # get    "/login",  to: 'sessions#new',     as: 'login'
   post   "/login",  to: 'sessions#create'
   delete "/logout", to: 'sessions#destroy', as: 'logout'
 
