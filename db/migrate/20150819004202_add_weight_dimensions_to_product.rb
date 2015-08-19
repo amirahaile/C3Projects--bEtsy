a1f568d54d9a5f4e93e59d97119fd990152fc1a7
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-18 19:43:19 -0700

    modified datatype of weight to integer

--- db/migrate/20150819004202_add_weight_dimensions_to_product.rb
+++ db/migrate/20150819004202_add_weight_dimensions_to_product.rb
@@ -1,8 +1,8 @@
 class AddWeightDimensionsToProduct < ActiveRecord::Migration
   def change
-    add_column :products, :weight_in_gms, :decimal, precision: 7, scale: 2
-    add_column :products, :length_in_cms, :decimal, precision: 7, scale: 2
-    add_column :products, :width_in_cms, :decimal, precision: 7, scale: 2
-    add_column :products, :height_in_cms, :decimal, precision: 7, scale: 2
+    add_column :products, :weight_in_gms, :integer
+    add_column :products, :length_in_cms, :decimal, precision: 6, scale: 1
+    add_column :products, :width_in_cms, :decimal, precision: 6, scale: 1
+    add_column :products, :height_in_cms, :decimal, precision: 6, scale: 1
   end
 end
