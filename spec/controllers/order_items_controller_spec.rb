bbf3877a232c4b29c408f5968df14859442b8dd1
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-18 21:35:30 -0700

    added new attributes to example product

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe OrderItemsController, type: :controller do
     before :each do
       order = Order.create
 
-      product1 = Product.create(name: "product1", price: 20.22, photo_url: "www.example.com", inventory: 20, user_id: 1)
-      product2 = Product.create(name: "product2", price: 46.12, photo_url: "www.example.com", inventory: 20, user_id: 1)
-      product3 = Product.create(name: "product3", price: 25.32, photo_url: "www.example.com", inventory: 20, user_id: 1)
+      product1 = Product.create(name: "product1", price: 20.22, photo_url: "www.example.com", inventory: 20, user_id: 1, weight_in_gms: 100, length_in_cms: 10, width_in_cms: 5, height_in_cms: 15)
+      product2 = Product.create(name: "product2", price: 46.12, photo_url: "www.example.com", inventory: 20, user_id: 1, weight_in_gms: 100, length_in_cms: 10, width_in_cms: 5, height_in_cms: 15)
+      product3 = Product.create(name: "product3", price: 25.32, photo_url: "www.example.com", inventory: 20, user_id: 1, weight_in_gms: 100, length_in_cms: 10, width_in_cms: 5, height_in_cms: 15)
 
       orderItem1 = OrderItem.create(quantity: 1, order_id: 1, product_id: 1)
       @orderItem2 = OrderItem.create(quantity: 1, order_id: 1, product_id: 2)
