dc4b25478d098846e486daacae64f9771444faae
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-19 22:37:15 -0700

    Refactored tests to use factory girl

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe "model validations" do
-    let(:category) { Category.new }
-
     context "name" do
       it "is required" do
-        expect(category).to_not be_valid
+        category = build :category, name: nil
+        category.valid?
         expect(category.errors.keys).to include(:name)
       end
 
       it "is unique" do
-        Category.create!(name: "Hats")
-        same_category = Category.new(name: "Hats")
-        expect(same_category).to_not be_valid
-        expect(same_category.errors.keys).to include(:name)
+        create :category
+        same_category = build :category, name: "Hats"
+        same_category.valid?
+        expect(same_category.errors.messages).to eq(:name => ["has already been taken"])
       end
     end
   end
