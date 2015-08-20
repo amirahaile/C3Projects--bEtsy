df6b0eb5497a483189da53ea8ff5784d94e2d062
Lindsey Reno <lindseyannereno@gmail.com>
2015-08-19 22:54:05 -0700

    Refactored to use factory girl

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -2,23 +2,27 @@ require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
   describe "validations" do
+    it "requires a quantity" do
+      order_item = build :order_item
+      order_item.valid?
+      expect(order_item.errors.keys).to_not include(:quantity)
+    end
 
-    # how can I test for presence when there's a default value?
-    it "defaults :quantity to 1" do
-      order_item = OrderItem.new(order_id: 1, product_id: 1)
-
-      expect(order_item.save).to be true
+    it "defaults quantity to 1" do
+      order_item = build :order_item
       expect(order_item.quantity).to eq 1
     end
 
-    it "doesn't allow :quantity to be a string" do
-      order_item = OrderItem.new(quantity: 'none', order_id: 1, product_id: 1)
-      expect(order_item.save).to be false
+    it "requires quantity to be a number" do
+      order_item = build :order_item, quantity: "three"
+      order_item.valid?
+      expect(order_item.errors.messages).to eq(:quantity => ["is not a number"])
     end
 
-    it "doesn't allow :quantity to be less than 0" do
-      order_item = OrderItem.new(quantity: -10, order_id: 1, product_id: 1)
-      expect(order_item.save).to be false
+    it "requires quantity to be greater than 0" do
+      order_item = build :order_item, quantity: -1
+      order_item.valid?
+      expect(order_item.errors.messages).to eq(:quantity => ["must be greater than 0"])
     end
   end
 end
