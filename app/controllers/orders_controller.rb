507868554195f0f561ee1e6f801aed5f7a545b37
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-21 13:44:36 -0700

    debugging redirect loop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -76,9 +76,9 @@ class OrdersController < ApplicationController
     elsif @calculated_rates.first.values.first == "422"
       redirect_to :shipping, notice: "Error in shipping choice. Please try again."
     elsif @calculated_rates.first.values.first == "408"
-      redirect_to root_path, notice: "We could not process your request in a timely manner. Please try again later."
+      redirect_to :shipping, notice: "We could not process your request in a timely manner. Please try again later."
     elsif @calculated_rates.first.values.first == "bad"
-      redirect_to :shipping, notice: "NOPE. Please try again."
+      redirect_to root_path, notice: "NOPE. Please try again."
     end
   end
 
