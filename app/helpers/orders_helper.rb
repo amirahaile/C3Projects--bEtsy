b6ad5e2f9ce0dd6cd40fe53d83bf8cfb74e2f833
amirahaile <amira.dhaile@gmail.com>
2015-08-20 20:34:34 -0700

    #usd_convert(cents) now pads with zeros\!

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -7,6 +7,13 @@ module OrdersHelper
 
   # TODO: Pad with zeros!
   def usd_convert(cents)
-    "$#{ cents / 100 }.#{ cents % 100 }"
+    dollars = cents / 100
+    cents = (cents % 100).to_s
+
+    while cents.length < 2
+      cents += "0"
+    end
+
+    "$#{ dollars }.#{ cents }"
   end
 end
